7f6ba337f62898ebb6527cb93391d0f8a99b0bb8
Dhruv <143328600+dhruvchaubey28@users.noreply.github.com>
2023-09-27 22:50:29 +0530

    Update house_price.py

--- house_price.py
+++ house_price.py
@@ -1,18 +1,21 @@
-import requests
-from bs4 import BeautifulSoup
+#importing important libraries
+import requests #for http requests
+from bs4 import BeautifulSoup  # for parsing html
+#creating a class 
 class Property_list_fetcher:
+  #define a method for a given location
   def fetch_property_listings(self, location):
         url = f"https://www.magicbricks.com/property-for-sale-rent-in-{location}-area"
         try:
             response = requests.get(url)
             response.raise_for_status()  # this will raise an exception if the request was unsuccessful
         except requests.exceptions.RequestException as E:
-            print(f"Error: {str(E)}")
+            print(f"Error: {str(E)}") #handle any request related errors
             return
   if response.status_code == 200:
             try:
                 soup = BeautifulSoup(response.content, 'html.parser')
-                property_listings = soup.find_all("div", class_="m-srp-card")
+                property_listings = soup.find_all("div", class_="m-srp-card") #find all properties on the page
 
                 if property_listings:
                     listings_text = []
@@ -23,8 +26,8 @@ class Property_list_fetcher:
                 else:
                     print(f"No property listings found for {location}")
                   except Exception as e:
-                print(f"Error parsing data: {str(e)}")
-        else:
+                print(f"Error parsing data: {str(e)}") #handle any parsing errors 
+        else: #will print error message if the https request fails
             print(f"Failed to retrieve data from Magicbricks.com for {location}")
           def main():
     fetcher = Property_list_fetcher()
@@ -35,7 +38,7 @@ class Property_list_fetcher:
         print("Please enter a location.")
 
 if __name__ == "__main__":
-    main()
+    main() #call the main function to start the program
 
 
             
