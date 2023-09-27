ef3648d0f4d6da19f8864ab50431367c95c9f70c
Dhruv <143328600+dhruvchaubey28@users.noreply.github.com>
2023-09-27 22:36:52 +0530

    Update house_price.py

--- house_price.py
+++ house_price.py
@@ -9,4 +9,17 @@ class Property_list_fetcher:
         except requests.exceptions.RequestException as E:
             print(f"Error: {str(E)}")
             return
-  
+  if response.status_code == 200:
+            try:
+                soup = BeautifulSoup(response.content, 'html.parser')
+                property_listings = soup.find_all("div", class_="m-srp-card")
+
+                if property_listings:
+                    listings_text = []
+                    for idx, listing in enumerate(property_listings, start=1):
+                        property_title = listing.find("span", class_="m-srp-card__title").text.strip()
+                        listings_text.append(f"{idx}. {property_title}")
+                    print("\n".join(listings_text))
+                else:
+                    print(f"No property listings found for {location}")
+            
