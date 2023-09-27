4799220150879438400094ed0e15b277e2360c6f
Dhruv <143328600+dhruvchaubey28@users.noreply.github.com>
2023-09-27 22:35:25 +0530

    Update house_price.py

--- house_price.py
+++ house_price.py
@@ -1,1 +1,12 @@
-
+import requests
+from bs4 import BeautifulSoup
+class Property_list_fetcher:
+  def fetch_property_listings(self, location):
+        url = f"https://www.magicbricks.com/property-for-sale-rent-in-{location}-area"
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # this will raise an exception if the request was unsuccessful
+        except requests.exceptions.RequestException as E:
+            print(f"Error: {str(E)}")
+            return
+  
